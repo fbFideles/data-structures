6645e514d4b52c9cbe4bcbcc906cc7cbe2b0f846
marcelobezer <marcelobez.g@gmail.com>
2020-10-30 11:30:28 -0300

    Add new type list; add list methods

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/marcelobezer/data-structures/list"
+
+func main() {
+	l := list.NewInt64List()
+	l.PushBack(1)
+	l.PushBack(5)
+	l.PushBack(6)
+
+	l.PrintList()
+	l.RPrintList()
+
+	l.PushFront(8)
+	l.PushFront(-6)
+	l.PushFront(0)
+
+	l.PrintList()
+	l.RPrintList()
+}
